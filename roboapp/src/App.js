63f2687975add21b084a01de30d34e8f9f6840ca
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-10-02 12:34:21 -0700

    Updated react components

--- roboapp/src/App.js
+++ roboapp/src/App.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './CardList';
-import { robots } from './robots'
+import Scroll from './Scroll'
+import SearchBox from './SearchBox';
+import './App.css';
 
 
-const App = () => {
+
+class App extends Component  {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then
+        (response => response.json())
+        .then(users => this.setState({ robots: users})) 
+       }
+
+    onSeachChange = (event) =>  {
+        this.setState({ searchfield: event.target.value })
+    }
+
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        })
+        if(this.state.robots.length === 0 ) {
+            return <h1>Loading</h1>
+        }else {
     return(
-        <CardList robots={robots}/>
-    )
+        <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox SeachChange={this.onSeachChange}/> 
+        <Scroll>
+        <CardList robots={filteredRobots}/>
+        </Scroll>
+        </div>
+        )
+    }
 }
-
+}
 export default App;
+
---
+++ roboapp/src/Scroll.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Scroll = (props) => {
+    console.log(props)
+    return props.children
+}
+
+export default Scroll 
