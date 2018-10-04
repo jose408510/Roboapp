3d6e74774ea902ae3273562b545eab7348de371d
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-10-04 10:25:07 -0700

    added redux

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Roboapp
+
+A Simple react App creating Robot profiles from fake Json API
+
+Getting Started
+Clone the repo to your local directory
+Cd into the project.
+NPM Install.
+Running the app
+Npm start.
+
--- roboapp/src/containers/App.js
+++ roboapp/src/containers/App.js
@@ -4,12 +4,31 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import { connect } from 'react-redux'
+import {  setSearchField , requestRobots } from '../actions'
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+ return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => 
+      requestRobots(dispatch)
+    }
+  }
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: [],
-      searchfield: ''
+      robots: []
     }
   }
 
@@ -19,21 +38,18 @@ class App extends Component {
       .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField , onSearchChange } = this.props; 
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -42,4 +58,6 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+export default connect( mapStateToProps, mapDispatchToProps)(App);
